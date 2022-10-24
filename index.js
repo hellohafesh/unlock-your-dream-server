763ca875279fc1db8cac8a26930a041a48750f6b
soumik825 <allhafesh825@gmail.com>
2022-10-25 05:08:39 +0600

    for cors problem add cors in index

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
+app.use(cors());
+const courses = require('./data/courses.json');
 app.get('/', (req, res) => {
     res.send('Running')
+})
+app.get('/courses', (req, res) => {
+    res.send(courses);
 })
 
 app.listen(port, () => {

ab02bb96606bbc2c9d1ddcf086d95381b31e9610
soumik825 <allhafesh825@gmail.com>
2022-10-25 21:00:16 +0600

    added some details data of our course list and send data with params id for find with id

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-
 app.use(cors());
+
+
 const courses = require('./data/courses.json');
+
+const coursesDetails = require('./data/details.json');
+
 app.get('/', (req, res) => {
     res.send('Running')
 })
+
 app.get('/courses', (req, res) => {
     res.send(courses);
+})
+
+app.get('/coursesDetails/:id', (req, res) => {
+
+    const id = req.params.id;
+    const yourCourse = coursesDetails.find(details => details.id === id);
+    res.send(yourCourse);
 })
 
 app.listen(port, () => {
